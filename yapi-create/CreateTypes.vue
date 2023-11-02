34e1cf10757606ab01d57beb812484c13a603444
shiouhoo <2972907073@qq.com>
2023-11-02 21:38:27 +0800

    feat: 新增处理表头顺序不一致

--- yapi-create/CreateTypes.vue
+++ yapi-create/CreateTypes.vue
@@ -10,18 +10,27 @@ const msgMap = {
     response: '返回数据',
 };
 
+const responsetableIndex = {
+    name: -1,
+    type: -1,
+    required: -1,
+    description: -1,
+};
+
 function getResponseTypes(panel: Element | null | undefined, level = 0) {
     let obj = '{\r\n';
     const trList = panel?.querySelectorAll(`.ant-table-body table tbody tr.ant-table-row-level-${level}`);
     Array.from(trList || []).forEach((tr) => {
-        const name = tr.querySelector('td:nth-child(1)')?.textContent;
-        let type = tr.querySelector('td:nth-child(2)')?.textContent?.replaceAll(' ', '');
-        if(type === 'object') {
+        const name = tr.querySelector(`td:nth-child(${responsetableIndex.name})`)?.textContent;
+        let type = tr.querySelector(`td:nth-child(${responsetableIndex.type})`)?.textContent?.replaceAll(' ', '');
+        if(type === 'integer') {
+            type = 'number';
+        }else if(type === 'object') {
             type = getResponseTypes(panel, level + 1);
         }else if(type === 'object[]') {
             type = getResponseTypes(panel, level + 1) + '[]';
         }
-        let description = tr.querySelector('td:nth-child(5)')?.textContent;
+        let description = tr.querySelector(`td:nth-child(${responsetableIndex.description})`)?.textContent;
         const tab = '    '.repeat(level + 1);
         description = description?.trim() ? `${tab}/** ${description} */\r\n` : '';
         const item = `${description}${tab}${name}: ${type};\r\n`;
@@ -36,24 +45,50 @@ const handleClick = () => {
     let panel = btnRef.value?.parentElement?.parentElement?.nextElementSibling;
     let obj = '{\r\n';
     if(type.value === 'request') {
+        let nameIndex = -1;
+        let typeIndex = -1;
+        let requiredIndex = -1;
+        let descriptionIndex = -1;
+        panel?.querySelectorAll('.ant-table-body .ant-table-thead tr th').forEach((item, index)=>{
+            if(item.textContent?.includes('参数名称')) {
+                nameIndex = index + 1;
+            }else if(item.textContent?.includes('类型')) {
+                typeIndex = index + 1;
+            }else if(item.textContent?.includes('是否必须')) {
+                requiredIndex = index + 1;
+            }else if(item.textContent?.includes('备注')) {
+                descriptionIndex = index + 1;
+            }
+        });
         const trList = panel?.querySelectorAll('.ant-table-body table tbody tr');
         Array.from(trList || []).forEach((tr) => {
-            const name = tr.querySelector('td:nth-child(1)')?.textContent;
-            let type = tr.querySelector('td:nth-child(2)')?.textContent;
+            const name = tr.querySelector(`td:nth-child(${nameIndex})`)?.textContent || '';
+            let type = tr.querySelector(`td:nth-child(${typeIndex})`)?.textContent || 'any';
             if(type?.includes('文本')) {
                 type = 'string';
             }
-            const required = tr.querySelector('td:nth-child(3)')?.textContent?.includes('是');
+            const required = tr.querySelector(`td:nth-child(${requiredIndex})`)?.textContent?.includes('是');
 
-            let description = tr.querySelector('td:nth-child(5)')?.textContent;
+            let description = tr.querySelector(`td:nth-child(${descriptionIndex})`)?.textContent;
             const descBeforeEnter = description?.includes('\n') ? '\r\n      * ' : ' ';
             const descEnter = description?.includes('\n') ? '\r\n      ' : ' ';
             description = description?.trim() ? `    /**${descBeforeEnter}${description?.replaceAll('\n', '\n      * ')}${descEnter}*/\r\n    ` : '';
             const item = `${description}${name}${required ? '' : '?'}: ${type};\r\n`;
             obj += item;
-            obj += '}';
         });
+        obj += '}';
     }else if(type.value === 'response') {
+        panel?.querySelectorAll('.ant-table-body .ant-table-thead tr th').forEach((item, index)=>{
+            if(item.textContent?.includes('名称')) {
+                responsetableIndex.name = index + 1;
+            }else if(item.textContent?.includes('类型')) {
+                responsetableIndex.type = index + 1;
+            }else if(item.textContent?.includes('是否必须')) {
+                responsetableIndex.required = index + 1;
+            }else if(item.textContent?.includes('备注')) {
+                responsetableIndex.description = index + 1;
+            }
+        });
         obj = getResponseTypes(panel);
     }
     navigator.clipboard.writeText(obj);
